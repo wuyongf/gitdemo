db571fadf8635ead717ebfb1e0b5a7d5592b57bb
wuyongf <wuyongf@gmail.com>
2021-02-26 17:55:13 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/gitdemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitdemo.dir/main.cpp.obj"
+  "gitdemo.exe"
+  "gitdemo.exe.manifest"
+  "gitdemo.lib"
+  "gitdemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
