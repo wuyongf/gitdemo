ee3206502e54864c3e9d3394f0a7abc510f5be45
wuyongf <wuyongf@gmail.com>
2021-02-27 00:03:00 +0800

    # v1.4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
